9dfe63b328d83b23463d92b61077a806a26f9121
cxx <xx.ch@outlook.com>
2017-11-17 17:54:05 +0800

    developing

---
+++ netbird-android-support/src/cc/colorcat/netbird3/android/AndroidScheduler.java
@@ -0,0 +1,23 @@
+package cc.colorcat.netbird3.android;
+
+import android.os.Handler;
+import android.os.Looper;
+import cc.colorcat.netbird3.platform.Scheduler;
+
+/**
+ * Created by cxx on 17-11-17.
+ * xx.ch@outlook.com
+ */
+public final class AndroidScheduler implements Scheduler {
+    private static final Handler HANDLER = new Handler(Looper.getMainLooper());
+
+    @Override
+    public void onTargetThread(Runnable runnable) {
+        HANDLER.post(runnable);
+    }
+
+    @Override
+    public boolean isTargetThread() {
+        return Looper.myLooper() == Looper.getMainLooper();
+    }
+}
