a51ec204140e42e6ccf1365accc2add68afdcf57
cxx <xx.ch@outlook.com>
2017-12-25 22:59:27 +0800

    non

--- netbird-android-support/src/cc/colorcat/netbird3/android/AndroidLogger.java
+++ netbird-android-support/src/cc/colorcat/netbird3/android/AndroidLogger.java
@@ -15,26 +15,26 @@ public class AndroidLogger implements Logger {
     public void log(@Level int level, String tag, String msg) {
         switch (level) {
             case Level.VERBOSE:
-                realPrintln(Log.VERBOSE, tag, msg);
+                println(Log.VERBOSE, tag, msg);
                 break;
             case Level.DEBUG:
-                realPrintln(Log.DEBUG, tag, msg);
+                println(Log.DEBUG, tag, msg);
                 break;
             case Level.INFO:
-                realPrintln(Log.INFO, tag, msg);
+                println(Log.INFO, tag, msg);
                 break;
             case Level.WARN:
-                realPrintln(Log.WARN, tag, msg);
+                println(Log.WARN, tag, msg);
                 break;
             case Level.ERROR:
-                realPrintln(Log.ERROR, tag, msg);
+                println(Log.ERROR, tag, msg);
                 break;
             default:
                 break;
         }
     }
 
-    private static void realPrintln(int priority, String tag, String msg) {
+    private static void println(int priority, String tag, String msg) {
         for (int start = 0, end = start + MAX_LENGTH, size = msg.length(); start < size; start = end, end = start + MAX_LENGTH) {
             if (end > size) {
                 Log.println(priority, tag, msg.substring(start));
